faa0ddbc630372a5b676c2361e795902693f2c9a
subrat0796 <subratsahoo0796@gmail.com>
2022-07-24 13:34:43 +0530

    Flipkart scraping done

--- server.js
+++ server.js
@@ -68,6 +68,59 @@ app.post("/amazon-product", async (req, res, next) => {
   }
 });
 
+app.post("/flipkart-products", async (req, res, next) => {
+  try {
+    const { item } = req.body;
+    const urlSearch = item.split(" ").join("%20");
+    const hosturl = `https://flipkart.com/`;
+    const url = `${hosturl}search?q=${urlSearch}`;
+
+    const { data } = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        "User-agent": "axois 0.21.1",
+      },
+    });
+
+    var $ = cherrio.load(data);
+
+    var result = [];
+
+    var dataItems = [],
+      imageItems = [],
+      nameItems = [],
+      priceItems = [];
+
+    $("a[class='_1fQZEK']").each(function () {
+      dataItems.push(hosturl + $(this).attr("href"));
+
+      // console.log($(this).attr("href"));
+    });
+
+    $("img[class='_396cs4 _3exPp9']").each(function () {
+      imageItems.push($(this).attr("src"));
+      nameItems.push($(this).attr("alt"));
+      // console.log($(this).attr("src"));
+    });
+
+    $("div[class='_30jeq3 _1_WHN1']").each(function () {
+      priceItems.push($(this).text());
+    });
+
+    return res.status(200).json({
+      message: [
+        { dataItems, size: dataItems.length },
+        { priceItems, size: priceItems.length },
+        { nameItems, size: nameItems.length },
+        { imageItems, size: imageItems.length },
+      ],
+    });
+  } catch (err) {
+    console.log(`Error while fetching flipkart data ----------> ${err}`);
+    return res.status(500).json({ message: "Some error occured" });
+  }
+});
+
 app.listen(5000, () => {
   console.log(`Server running on port ${5000}`);
 });
